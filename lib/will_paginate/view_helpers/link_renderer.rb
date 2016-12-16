3d6eb68dff9fda9bdd12b680177d566f35c75dc8
khoatran <khoatranthanh.it@gmail.com>
2016-12-16 16:26:56 +0700

    Refactor view

--- lib/will_paginate/view_helpers/link_renderer.rb
+++ lib/will_paginate/view_helpers/link_renderer.rb
@@ -42,7 +42,7 @@ module WillPaginate
     
       def page_number(page)
         if page == current_page
-          tag(:li, page, :class => 'active')
+          tag(:li, "<a>"+page+"</a>", :class => 'active')
         else
           link(page, page, :rel => rel_value(page))
         end
